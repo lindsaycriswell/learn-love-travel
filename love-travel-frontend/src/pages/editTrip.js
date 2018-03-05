906e9c1d7be361389ed8eec7d3da9a03aee350cc
brianabaker <brianaclairbaker@gmail.com>
2018-03-05 12:34:56 -0500

    maybe

---
+++ love-travel-frontend/src/pages/editTrip.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+
+
+class EditTrip extends React.Component {
+  render(){
+    return(
+      <div>
+        hiii
+      </div>
+    )
+  }
+}
+
+export default EditTrip
--- love-travel-frontend/src/pages/yourTrips.js
+++ love-travel-frontend/src/pages/yourTrips.js
@@ -1,9 +1,14 @@
 
-import React from 'react'
-let moment = require('moment');
-// moment().format();
 
-// i need this to show the trips after they're made
+
+// react stuff needed
+import React from 'react'
+
+// components
+import EditTrip from './editTrip'
+
+// this needs to go under the components
+let moment = require('moment');
 
 class YourTrips extends React.Component  {
   state = {
@@ -22,18 +27,25 @@ class YourTrips extends React.Component  {
 // /users/:user_id/yourTrips
 
   render() {
-    // {this.fetchYourTrips()}
-    // console.log(this.state.yourTrips)
-    // console.log(this.props.currentUser)
-    // this.state.yourTrips.length < 0 ?
-     // : "No Trips Yet!! Go book some!"}
+    //need to sort by most recent first
     return(
-      <div>
-          {this.state.yourTrips.map(trip =>
-            <div className="item">
-              <p>{moment(trip.start_date).format("MMM Do YY")} to {moment(trip.end_date).format("MMM Do YY")}</p>
-              <p>{trip.location.name}</p>
-            </div>)}
+      <div className="ui grid" style={{paddingTop: "30px"}}>
+        <div className="three wide column"></div>
+        <div className="four wide column">
+      <div className="ui items">
+          {this.state.yourTrips.length > 0 ? this.state.yourTrips.map(trip =>
+            <div key={trip.id} className="item">
+              <div key={trip.id} className="content">
+                <div className="ui header">{trip.location.name}</div>
+                <div className="description">{moment(trip.start_date).format("MMM Do")} <i className="arrow right icon"></i> {moment(trip.end_date).format("MMM Do YY")}</div>
+                <div>{trip.notes.length > 0 ? trip.notes : "Add Some Notes!" }</div>
+                <button className="tiny green ui button" onClick={() => this.handleEditClick(trip)}>Edit Trip</button>
+              </div>
+            </div>) : <div className="ui header" style={{paddingTop: "40px"}}> No Trips Yet! </div> }
+          </div>
+          </div>
+          <div className="nine wide column">
+          {this.state.editTrip ? <EditTrip user={this.props.currentUser.id} data={this.state.editTrip} />: null }</div>
       </div>
     )}
 
