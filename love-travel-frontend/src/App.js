60781ac16afbd89c89dcddea9ea4a5d552e6661c
github username <github email address>
2018-03-05 10:11:55 -0500

    location photos

--- love-travel-frontend/src/App.js
+++ love-travel-frontend/src/App.js
@@ -19,11 +19,8 @@ import FirstSignUpPage from "./pages/firstSignUpPage";
 import Welcome from "./pages/welcome";
 import MakeTrip from "./pages/makeTrip";
 import Location from "./Location";
-<<<<<<< Updated upstream
-import YourTrips from './pages/yourTrips'
-=======
+import YourTrips from "./pages/yourTrips";
 import AttractionDetail from "./AttractionDetail";
->>>>>>> Stashed changes
 // import AttractionList from "./attractionList";
 
 class App extends React.Component {
@@ -58,12 +55,14 @@ class App extends React.Component {
     });
   };
 
-  setCity = (cityData) => {
-    this.setState({
-      city: cityData
-    }, () => console.log(this.state.city))
-  }
-
+  setCity = cityData => {
+    this.setState(
+      {
+        city: cityData
+      },
+      () => console.log(this.state.city)
+    );
+  };
 
   findLocation = routerParams => {
     return this.state.locations.find(function(location) {
@@ -98,37 +97,76 @@ class App extends React.Component {
               );
             }}
           />
-          <Route exact path='/' render={props => (
-              <Home {...props} currentUser={this.state.currentUser} setCurrentUser={this.setCurrentUser}/>
-            )} />
-            <Route
-              exact
-              path="/map"
-              render={props => (
-                <MapContainer
-                  {...props}
-                  setCity={this.setCity}
-                  currentUser={this.state.currentUser}
-                  locations={this.state.locations}
-                />
-              )}
-            />
-          <Route exact path='/about' component={About}/>
-          <Route exact path='/signup' render={props => (
-              <SignUp {...props} currentUser={this.state.currentUser} setCurrentUser={this.setCurrentUser}/>
-            )} />
-          <Route exact path='/firstSignUpPage' render={props => (
-              <FirstSignUpPage {...props} currentUser={this.state.currentUser} />
-            )} />
-          <Route exact path='/welcome' render={props => (
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <Home
+                {...props}
+                currentUser={this.state.currentUser}
+                setCurrentUser={this.setCurrentUser}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/map"
+            render={props => (
+              <MapContainer
+                {...props}
+                setCity={this.setCity}
+                currentUser={this.state.currentUser}
+                locations={this.state.locations}
+              />
+            )}
+          />
+          <Route exact path="/about" component={About} />
+          <Route
+            exact
+            path="/signup"
+            render={props => (
+              <SignUp
+                {...props}
+                currentUser={this.state.currentUser}
+                setCurrentUser={this.setCurrentUser}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/firstSignUpPage"
+            render={props => (
+              <FirstSignUpPage
+                {...props}
+                currentUser={this.state.currentUser}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/welcome"
+            render={props => (
               <Welcome {...props} currentUser={this.state.currentUser} />
-            )} />
-          <Route exact path='/makeTrip' render={props => (
-              <MakeTrip {...props} currentUser={this.state.currentUser} currentCity={this.state.city}/>
-            )} />
-          <Route exact path='/yourTrips' render={props => (
-              <YourTrips {...props} currentUser={this.state.currentUser}/>
-              )} />
+            )}
+          />
+          <Route
+            exact
+            path="/makeTrip"
+            render={props => (
+              <MakeTrip
+                {...props}
+                currentUser={this.state.currentUser}
+                currentCity={this.state.city}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/yourTrips"
+            render={props => (
+              <YourTrips {...props} currentUser={this.state.currentUser} />
+            )}
+          />
         </Switch>
       </div>
     );
--- love-travel-frontend/src/Location.js
+++ love-travel-frontend/src/Location.js
@@ -1,19 +1,36 @@
 import React from "react";
 import Attraction from "./Attraction";
+import Unsplash from "unsplash-js";
 
-const Location = props => {
-  return (
-    <div>
-      {console.log(props)}
-      <h1>{props.location.name}</h1>
-      <div className="ui relaxed divided list">
-        <h3>Popular Attractions</h3>
-        {props.location.attractions.map(attraction => (
-          <Attraction attraction={attraction} key={attraction.id} />
-        ))}
+const unsplash = new Unsplash({
+  applicationId:
+    "129e1c37805baaa3c936aaea1cd984828281182dc4440f5e8406b421c0b6b0e4",
+  secret: "8f957a7929701b86d8365960b8830ded0cca2d0fcc8f7c0a3782f10a3a80b9b2",
+  callbackUrl: "urn:ietf:wg:oauth:2.0:oob"
+});
+
+class Location extends React.Component {
+  componentDidMount() {
+    unsplash.search
+      .photos(`${this.props.location.name}`)
+      .then(res => res.json())
+      .then(json => console.log(json.results));
+  }
+
+  render() {
+    return (
+      <div>
+        {console.log(this.props.location)}
+        <h1>{this.props.location.name}</h1>
+        <div className="ui relaxed divided list">
+          <h3>Popular Attractions</h3>
+          {this.props.location.attractions.map(attraction => (
+            <Attraction attraction={attraction} key={attraction.id} />
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Location;
